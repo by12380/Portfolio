4369829f057ebb569dc8fd92881a8b8244dae96d
Brian Young <by12380@gmail.com>
2018-10-15 23:57:18 -0700

    Refactor: move landing page to component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
     <Navbar id="navbar" :activePage="activePage" :isTop="isTop" v-scroll-reveal="{ origin: 'top' }"/>
-    <div id="home" class="landing-page">
-      <div class="bg"></div>
-      <div class="profile-wrapper" v-scroll-reveal>
-        <img alt="Vue logo" src="./assets/images/profile.jpg"/>
-      </div>
-      <h1 class="name" v-scroll-reveal="{ origin: 'left', delay: 1000 }">BRIAN YOUNG</h1>
-      <h2 class="title" v-scroll-reveal="{ origin: 'left', delay: 1000 }">Software Developer</h2>
-    </div>
+    <Home id="home" />
     <About id="about" />
     <Applications id="applications" />
     <Contact id="contact" />
@@ -17,6 +10,7 @@
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Applications from './components/Applications.vue'
 import Contact from './components/Contact.vue'
@@ -26,6 +20,7 @@ export default {
   name: 'app',
   components: {
     Navbar,
+    Home,
     About,
     Applications,
     Contact
@@ -100,49 +95,6 @@ body {
   z-index: 9999;
 }
 
-.landing-page {
-  min-height: 100vh;
-  padding-top: 100px;
-  position: relative;
-  box-shadow: 0 1px 30px rgb(190, 190, 190);
-}
-
-.bg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-image:
-    url("./assets/images/newport-beach.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-  -webkit-filter: opacity(0.6);
-  filter: opacity(0.6);
-  z-index: -9999;
-  box-shadow: 0 80px 200px 50px #faf8f6 inset;
-}
-
-.profile-wrapper img{
-  height: 200px;
-  border-radius: 50%;
-  border: solid 8px rgb(255, 255, 255);
-}
-
-.name {
-  font-size: 70px;
-  margin-bottom: 0;
-  text-shadow: 0px 3px 20px white;
-  color: #243342;
-}
-
-.title {
-  font-size: 30px;
-  margin: 0;
-  text-shadow: 0px 3px 20px white;
-  color: #243342;
-}
 
 .v-scroll-reveal {
   opacity: 1 !important;
---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="landing-page">
+      <div class="bg"></div>
+      <div class="profile-wrapper" v-scroll-reveal>
+        <img alt="Vue logo" src="../assets/images/profile.jpg"/>
+      </div>
+      <h1 class="name" v-scroll-reveal="{ origin: 'left', delay: 1000 }">BRIAN YOUNG</h1>
+      <h2 class="title" v-scroll-reveal="{ origin: 'left', delay: 1000 }">Software Developer</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+.landing-page {
+  min-height: 100vh;
+  padding-top: 100px;
+  position: relative;
+  box-shadow: 0 1px 30px rgb(190, 190, 190);
+}
+
+.bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image:
+    url("../assets/images/newport-beach.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  -webkit-filter: opacity(0.6);
+  filter: opacity(0.6);
+  z-index: -9999;
+  box-shadow: 0 80px 200px 50px #faf8f6 inset;
+}
+
+.profile-wrapper img{
+  height: 200px;
+  border-radius: 50%;
+  border: solid 8px rgb(255, 255, 255);
+}
+
+.name {
+  font-size: 70px;
+  margin-bottom: 0;
+  text-shadow: 0px 3px 20px white;
+  color: #243342;
+}
+
+.title {
+  font-size: 30px;
+  margin: 0;
+  text-shadow: 0px 3px 20px white;
+  color: #243342;
+}
+</style>
+
+
