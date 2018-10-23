07e6e31ff15e0df57865cc446a5e23d40ea122f3
Brian Young <by12380@gmail.com>
2018-10-23 15:13:06 -0700

    Get movies from tmdb

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,14 +1,214 @@
 <template>
-    <p>Movies</p>
+    <div class="movie-section container-fluid">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <h1 class="header" v-scroll-reveal="{ origin: 'left' }">My Movie Collections</h1>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+        <div id="container" v-if="movies">
+            <div
+                :class="[(currentIndex === index) ? 'active' : '', 'image-container']"
+                v-for="(movie, index) in movies.items"
+                :key="index"
+                ref="movies"
+                @click="goTo(index)" >
+                <img :src="computedImageUrl(movie.poster_path)" />
+                <div class="bottom-stripe" v-if="currentIndex === index"></div>
+                <i class="fas fa-heart" v-if="currentIndex === index"></i>
+            </div>
+            <button class="next-btn" @click="goNext()">
+                <i class="fas fa-arrow-right"></i>
+            </button>
+            <button class="back-btn" @click="goBack()">
+                <i class="fas fa-arrow-left"></i>
+            </button>
+        </div>
+        <div class="lower-section">
+            <div class="row">
+                <div class="col-12" style="background-color: white">
+                    <h2>Press <i class="fas fa-heart"></i> if you also like the movie</h2>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import { TMDB_API_URL, TMDB_API_KEY, TMDB_MOVIE_LIST_ID } from '../config.js';
+import $ from 'jquery';
+
 export default {
-    name: 'Movies'
+    name: 'Movies',
+    data: function() {
+        return {
+            movies: null,
+            prevIndex: 0,
+            currentIndex: 0
+        }
+    },
+    methods: {
+        getMovies: function() {
+            this.$http.get(`https://${TMDB_API_URL}/3/list/${TMDB_MOVIE_LIST_ID}?api_key=${TMDB_API_KEY}&language=en-US`)
+            .then((response) => {
+                this.movies = response.body;
+            })
+        },
+        computedImageUrl: function(path) {
+            return `https://image.tmdb.org/t/p/w300${path}`;
+        },
+        goTo: function(index) {
+            this.prevIndex = this.currentIndex;
+            this.currentIndex = index;
+            let screenWidth = window.innerWidth;
+            let leftPosition = this.$refs.movies[this.currentIndex].offsetLeft;
+            console.log(this.$refs.movies);
+            let targetPosition
+                = (this.currentIndex > this.prevIndex )
+                ? (leftPosition - screenWidth / 2 + 175)
+                : (leftPosition - screenWidth / 2 + 145);
+            $('#container').animate({scrollLeft: targetPosition}, 500)
+        },
+        goNext: function() {
+            if (this.currentIndex < (this.$refs.movies.length - 1)) {
+                this.goTo(this.currentIndex + 1);
+            }
+        },
+        goBack: function() {
+            if (this.currentIndex > 0) {
+                this.goTo(this.currentIndex - 1);
+            }
+        }
+    },
+    created() {
+        this.getMovies();
+    }
 }
 </script>
 
 <style scoped>
+
+    .movie-section {
+        text-align: left;
+    }
+
+    .movie-section .header {
+        margin: 0;
+        display: inline-block;
+        margin-top: 60px;
+        padding-bottom: 10px;
+        position: relative;
+    }
+
+    .movie-section .header::after {
+        content: "";
+        display: inline-block;
+        width: 50px;
+        height: 5px;
+        background-color: red;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+
+    #container {
+        display: flex;
+        align-items: center;
+        padding: 20px;
+        margin-top: 50px;
+        overflow: auto;
+        white-space: nowrap;
+    }
+
+    .image-container {
+        box-sizing: border-box;
+        display: inline-block;
+        padding: 60px;
+        height: 400px;
+        cursor: pointer;
+        position: relative;
+    }
+
+    .image-container img {
+        height: 100%
+    }
+
+    .active {
+        padding: 10px;
+    }
+
+    .next-btn {
+        position: absolute;
+        right: 10px;
+        background-color: white;
+        border: none;
+        font-size: 30px;
+        opacity: 0.6;
+        cursor: pointer;
+        transition: 0.2s ease-in;
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }
+
+    .next-btn:focus {
+        outline: none;
+    }
+
+    .next-btn:hover {
+        opacity: 0.9;
+    }
+
+    .back-btn {
+        position: absolute;
+        left: 10px;
+        background-color: white;
+        border: none;
+        font-size: 30px;
+        opacity: 0.6;
+        cursor: pointer;
+        transition: 0.2s ease-in;
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }
+
+    .back-btn:focus {
+        outline: none;
+    }
+
+    .back-btn:hover {
+        opacity: 0.9;
+    }
+
+    .lower-section {
+        text-align: center;
+        margin-top: -20px;
+    }
+
+    .lower-section h2 {
+        padding-top: 50px;
+    }
+
+    .fa-heart {
+        color: red;
+    }
+
+    .bottom-stripe {
+        height: 40px;
+        width: 100%;
+        background-color: black;
+        bottom: 46px;
+        position: relative;
+        opacity: 0.5;
+    }
+
+    .image-container .fa-heart {
+        font-size: 30px;
+        position: absolute;
+        bottom: 15px;
+        left: 20px;
+        color: rgb(255, 83, 83);
+    }
 
 </style>
 
