da5f752a4f80fd9181d7f09aea0019ea9f181208
Brian Young <by12380@gmail.com>
2018-11-04 14:05:16 -0500

    Replace resume btn with arrow btn

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,40 @@
 <template>
     <div class="landing-page">
       <div class="bg"></div>
-      <div class="profile-wrapper" v-scroll-reveal>
-        <img alt="Vue logo" src="../assets/images/profile.jpg"/>
-      </div>
-      <h1 class="name" v-scroll-reveal="{ origin: 'left', delay: 1000 }">BRIAN YOUNG</h1>
-      <h2 class="title" v-scroll-reveal="{ origin: 'left', delay: 1000 }">Software Developer</h2>
-      <a
-        href="https://www.dropbox.com/s/ggn6e7qs46mpl98/Resume_Brian%20Young.pdf?raw=1"
-        v-scroll-reveal="{ origin: 'left', delay: 1000 }"
-        target="_blank">
-        <button class="resume-btn">DOWNLOAD RESUME</button>
-      </a>
+      <main>
+        <div>
+          <div class="profile-wrapper" v-scroll-reveal>
+            <img alt="Vue logo" src="../assets/images/profile.jpg"/>
+          </div>
+          <h1 class="name" v-scroll-reveal="{ origin: 'left', delay: 1000 }">BRIAN YOUNG</h1>
+          <h2 class="title" v-scroll-reveal="{ origin: 'left', delay: 1000 }">Software Developer</h2>
+        </div>
+        <div class="bottom-row" v-scroll-reveal="{ origin: 'top', delay: 1000 }">
+          <i class="fas fa-arrow-down" @click="onArrowClick()"></i>
+        </div>
+      </main>
     </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    methods: {
+      onArrowClick: () => {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top
+        }, 500);
+      }
+    }
 }
 </script>
 
 <style scoped>
 .landing-page {
-  min-height: 100vh;
+  min-height: 600px;
+  height: 100vh;
   padding-top: 100px;
   position: relative;
   box-shadow: 0 1px 30px rgb(190, 190, 190);
@@ -46,6 +57,19 @@ export default {
   box-shadow: 0 80px 200px 50px #faf8f6 inset;
 }
 
+main {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.bottom-row {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+}
+
 .profile-wrapper img{
   height: 170px;
   border-radius: 50%;
@@ -66,48 +90,18 @@ export default {
   color: #243342;
 }
 
-.resume-btn {
-  border-radius: 0;
-  border: solid 2px #1D2053;
-  font-size: 18px;
-  font-weight: 500;
-  padding: 10px 20px 10px 20px;
-  margin-top: 50px;
-  margin-bottom: 50px;
-  color:#1D2053;
-  background-color: transparent;
-  position: relative;
-  cursor: pointer;
-  text-shadow: 0px 3px 10px white;
-  box-shadow: 0 2px 4px rgb(122, 122, 122);
-  transition: 0.2s ease-in;
-}
-
-.resume-btn::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: white;
-  opacity: 0.3;
-  z-index: -99;
-  transition: 0.2s ease-in;
-}
-
-.resume-btn:hover {
-  border: solid 2px rgb(48, 86, 136);
-  color:rgb(48, 86, 136);
-  box-shadow: 0 2px 4px rgb(73, 73, 73);
-}
-
-.resume-btn:hover::before {
-  opacity: 0.5;
-}
-
 button:focus {
   outline: none;
+}
+
+.fa-arrow-down {
+  font-size: 46px;
+  cursor: pointer;
+  transition: 150ms ease-in;
+}
+
+.fa-arrow-down:hover {
+  color: rgb(108, 152, 196);
 }
 </style>
 
